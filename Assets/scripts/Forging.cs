9b77287a7783bd04c8f9547c7fb58d7c31073482
AKU <jonghunchae4@gmail.com>
2025-04-08 23:02:28 +0900

    Inevntory Name Change, Sound add

--- Assets/scripts/Forging.cs
+++ Assets/scripts/Forging.cs
@@ -8,17 +8,18 @@ public class Forging : MonoBehaviour
     public TMPro.TextMeshProUGUI weaponName;
     public TMPro.TextMeshProUGUI weaponValue;
     public ParticleSystem forgingParticles;
-    public Animator invController;
-    public GameObject OpenButton;
-    public GameObject CloseButton;
-    
+    public Animator menuController;
+    public GameObject openButton;
+    public GameObject closeButton;
+    public AudioSource forgingAudioSource;
+    public AudioClip forgingSound;
     public void Start() 
     {
-        CloseButton.SetActive(false);
-        WeapomInfo("단조된 금속 덩어리", 0);    
+        closeButton.SetActive(false);
+        WeaponInfo("단조된 금속 덩어리", 0);    
     }
 
-    public void WeapomInfo(string name,int value)
+    private void WeaponInfo(string name,int value)
     {
         weaponName.text = $"{name}";
         weaponValue.text = $"{value}";
@@ -26,21 +27,22 @@ public class Forging : MonoBehaviour
 
     public void OnclickForging()
     {
+        forgingAudioSource.PlayOneShot(forgingSound);
         forgingParticles.Play();
     }
 
-    public void OnclickInvntoryOpen()
+    public void OnclickMenuOpen()
     {
-        OpenButton.SetActive(false);
-        invController.SetTrigger("Open");
-        CloseButton.SetActive(true);
+        openButton.SetActive(false);
+        menuController.SetTrigger("Open");
+        closeButton.SetActive(true);
     }
     
-    public void OnclickInvntoryClose()
+    public void OnclickMenuClose()
     {
-        CloseButton.SetActive(false);
-        invController.SetTrigger("Close");
-        invController.SetTrigger("Return");
-        OpenButton.SetActive(true);
+        closeButton.SetActive(false);
+        menuController.SetTrigger("Close");
+        menuController.SetTrigger("Return");
+        openButton.SetActive(true);
     }
 }
